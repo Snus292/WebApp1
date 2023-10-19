355592fcb0098acff800df027f825d3592dc10d8
Snus292 <georgiigiblov@gmail.com>
2023-10-19 09:00:00 +0300

    777

---
+++ js/script2.js
@@ -0,0 +1,431 @@
+window.addEventListener("DOMcontentLoaded",function(){
+    //Tabs
+    let tabs =document.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent"),
+        tabsParent = document.querySelector(".tabheader__items");
+    
+    function hideTabContent(){
+
+        tabsContent.forEach(item =>{
+            item.classList.add("hide");
+            item.classList.remove("show","fade");
+        });
+
+        tabs.forEach(item =>{
+            item.classList.remove("tabheader__item_active");
+
+        });
+
+    }
+    function showTabContent(i = 0){
+        tabsContent[i].classList.add("show","fade");
+        tabsContent[i].classList.remove("hide");
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
+    hideTabContent();
+    showTabContent();
+
+
+    tabsParent.addEventListener("click",function(event){
+        const target = event.target;
+        if (target && target.classList.contains("tabheader__item")){
+            tabs.forEach((item, i)=>{
+                if(target == item){
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+    // Timer
+
+    const deadline = "2021-12-11";
+
+    function getTimeRemaining(endtime){
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor( (t/(1000*60*60*24)) ),
+            seconds = Math.floor( (t/1000) % 60),
+            minutes = Math.floor( (t/1000/60) % 60),
+            hours = Math.floor( (t/(1000*60*60) % 24) );
+            
+        return{
+            "total": t,
+            "days": days,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds
+        };
+    }
+
+    function getZero(num){
+        if (num >= 0 && num < 10){
+            return "0" + num;
+        }else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime){
+
+        const timer = document.querySelector(selector),
+            days =timer.querySelector("#days"),
+            hours=timer.querySelector("#hours"),
+            minutes=timer.querySelector("#minutes"),
+            seconds=timer.querySelector("#seconds"),
+            timeInterval = setInterval(updateClock,1000);
+        
+        updateClock();
+
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock(".timer", deadline);
+
+
+    //Modal
+
+    const modalTrigger = document.querySelectorAll("[data-modal]"),
+        modal =  document.querySelector.apply(".modal");
+    
+    modalTrigger.forEach(btn => {
+        btn.addEventListener("click",openModal);
+    });
+
+    function closeModal(){
+        modal.classList.add("hide");
+        modal.classList.remove("show");
+        document.body.style.overflow = "";
+    }
+    function openModal(){
+        modal.classList.add("show");
+        modal.classList.remove("hide");
+        document.body.style.overflow = "hidden";
+        clearInterval(modalTimerId);
+    }
+    modal.addEventListener("click",(e)=> {
+        if (e.target === modal || e.target.getAttribute("data-close")== ""){
+            closeModal();
+        }
+    });
+
+    document.addEventListener("keydown", (e)=>{
+        if (e.code === "Escape" && modal.classList.contains("show")){
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal,300000);
+
+    function showModalByScroll(){
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+            openModal();
+            window.removeEventListener("scroll",showModalByScroll);
+
+        }
+    }
+    window.addEventListener("scroll", showModalByScroll);
+
+
+    // Используем классы для создания крарточек меню
+
+    class MenuCard {
+        constructor(src,alt,title,descr,price,parentSelector,...classes){
+            this.src =src;
+            this.alt =alt;
+            this.title =title;
+            this.descr =descr;
+            this.price =price;
+            this.classes =classes;
+            this.parent = document.querySelector(parentSelector);
+            this.trasfer = 1;
+            this.changeToUSD();
+        }
+
+        changeToUSD(){
+            this.price= this.price * this.trasfer;
+        }
+        render(){
+            const element =document.createElement("div");
+
+            if (this.classes.length === 0){
+                this.classes = "menu__item";
+                element.classList.add(thiss.classes);
+            }else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">{this.title}</h3>
+                <div class="menu__item-descr">{this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Cost</div>
+                    <div class="menu__item-total"><span>${this.price}</span>EUR/day</div>
+                </div>
+            `;
+            this.parent.append(element);     
+        }
+    }
+    getResourse("http://localhost:3000/menu")
+        .then(data =>{
+            data.forEach(({img,altimg,title,descr,price})=>{
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+        });
+
+    // new MenuCard(
+    //     "img/tabs/vegy.jpg",
+    //     "vegy",
+    //     "Меню 'Фитнес'",
+    //     "Меню 'Фитнес' - это новый подход к приготовлению блюд: большеу свежих овощей и фруктов." +
+    //     " Продукт активных и  здоровый людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+    //     9,
+    //     ".menu .container"
+
+    // ).render();
+
+    // new MenuCard(
+    //     "img/tabs/post.jpg",
+    //     "post",
+    //     "Меню 'Постное'",
+    //     "Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения," +
+    //     " молоко из миндаля, овса, кокоса или гречки, провильное количество белков засчет тофу и импортных "+
+    //     " вегатерианских стейков.",
+    //     14,
+    //     ".menu .container"
+    // ).render();
+
+    // new MenuCard(
+    //     "img/tabs/elite.jpg",
+    //     "elite",
+    //     "Меню Премиум'",
+    //     "В меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд" +
+    //     " Красная рыба ,морепродукты ,фрукты - ресторанное меню без похода в ресторан!",
+    //     21,
+    //     ".menu .container"
+    // ).render();
+
+        // Forms
+
+    const forms = document.querySelector("form");
+    const message = {
+        loading: "img/form/spinner.svg",
+        success: "Спасибо! Скроро мы свами свяжемся",
+        failure: "Что-то пошло не так..."
+    };
+
+    forms.forEach(item =>{
+        postData(item);
+    });
+
+    const postData = async(url,data)=>{
+        let res =await fetch(url,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:data
+        });
+
+        return await res.json();
+    };
+    async function getResourse(url){
+        let res =await fetch(url);
+
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    function bindPostData(form){
+        form.addEventListener("submit", (e) =>{
+            e.preventDefault();
+
+            let statusMessage = document.createElement("img");
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement("afterend",statusMessage);
+
+            const formData = new FormData(form);
+
+            const json =JSON.stringify(object.fromEntries(formData.entries()));
+
+            postData("http://localhost:3000/request",json)
+             
+            // })
+
+            // const object = {};
+            // formData.forEach(function (value,key){
+            //     object[key]=value;
+            // });
+
+            // fetch("server.php",{
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "application/json"
+            //     },
+            //     body: JSON.stringify(object)
+
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            }).catch(()=>{
+                showThanksModal(message.failure);
+            }).finally(()=> {
+                form.reset();
+            });
+
+        });
+    }
+
+    function showThanksModal(message){
+        const prevModalDialog = document.querySelector(".modal__dialog");
+
+        prevModalDialog.classList.add("hide");
+        openModal();
+
+        const thanksModal = document.createElement("div");
+        thanksModal.classList.add("modal__dialog");
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div class="modal__close" data-close>x</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+        document.querySelector(".modal").append(thanksModal);
+        setTimeout(()=>{
+            thanksModal.remove();
+            prevModalDialog.classList.add("show");
+            prevModalDialog.classList.remove("hide");
+            closeModal();
+        },4000);   
+    }
+    // Slider
+
+    let offset = 0;
+    let slideIndex = 1;
+
+    const slides =document.querySelectorAll(".offer__slide"),
+        slider =document.querySelector(".offer__slider"),
+        prev = document.querySelector(".offer__slider-prev"),
+        next = document.querySelector(".offer__slider-next"),
+        total = document.querySelector("#total"),
+        current = document.querySelector("#current"),
+        slidesWrapper = document.querySelector(".offer__slider-wrapper"),
+        width = window.getComputedStyle(slidesWrapper).width,
+        slidesField = document.querySelector(".offer__slider-inner");
+
+    if (slider.length < 10){
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    }else{
+        total.textContent =slides.length;
+        current.textContent = slideIndex;
+    }
+
+    slidesField.style.width = 100 * slides.length + "%";
+    slidesField.style.display = "flex";
+    slidesField.style.transition = "0.5s all";
+
+    slidesWrapper.style.overflow ="hidden";
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    slider.style.position = "relative";
+
+    const indicators = document.createElement("ol"),
+        dots= [];
+    indicators.classList.add("carousel-indicators");
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right:  15%;
+        margin-left: 15%;
+        list-style: none;
+
+    `; 
+    // Добавить стили  по желанию
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++){
+        const dot = document.createElement("li");
+        dot.setAttribute("data-slide-to",i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: white;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0){
+            dot.style.opacity =1;
+
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    next.addEventListener("click", ()=>{
+        if (offset == (deleteNotDigits(width) * (slides.length - 1))){
+            offset = 0;
+        }else{
+            offset += deleteNotDigits(width);
+        }
+        
+        slidesField.style.transfotm = `translateX(-${offset}px`;
+
+        if(slideIndex == slides.length){
+            slideIndex = 1;
+        }else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10 ){
+            current.textContent = `0${slideIndex}`;
+        }else{
+            current.textContent = slideIndex;
+        }
+
+        dots.forEach(dot => dot.style.opacity = ".5");
+        dots[slideIndex-1].style.opacity = 1;
+    });
+
+    prev.addEventListener("click")
+});
+    
+
+
--- server.php
+++ server.php
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 <?php
 $_POST = json_decode( file_get_contents("php://input"),true);
-=======
-<?php
-$_POST = json_decode( file_get_contents("php://input"),true);
->>>>>>> 61070fc (Исправленный ошибки и скоректирована 3 часть)
 echo var_dump($_POST);
